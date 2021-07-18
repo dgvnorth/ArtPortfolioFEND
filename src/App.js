d28b2688435dba260bc912d6efb06b15bc8b3a0b
dgvnorth <campello03@hotmail.com>
2021-07-18 10:22:37 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import HomeScreen from './screens/HomeScreen';
+import LonginScreen from './screens/LoginScreen';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={HomeScreen} />
+          <Route exact path="/login" component={LonginScreen} />
+        </Switch>
+      </BrowserRouter>
+    </>
+
   );
 }
 
---
+++ src/screens/HomeScreen/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+
+const HomeScreen = () => {
+    return (
+        <div>
+            <h1>
+                <Button variant="light">Light</Button> <Button variant="dark">Dark</Button>{' hola'}
+            </h1>
+        </div>
+    )
+}
+
+export default HomeScreen;
